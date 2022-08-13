4633ce7afd416ec82005e3201fa5166d580bfb73
Eugenia Palamarenko <palamarenko.evgeniya@gmail.com>
2022-08-14 00:33:37 +0300

    first comment

---
+++ technoshop-cms/script/modalController.js
@@ -0,0 +1,47 @@
+import { form, modalTitle, modal, modalSubmitBtn } from './elems.js';
+import { hidePreview } from './previewController.js';
+import { fillingForm } from './formController.js';
+
+const openModal = (id) => {
+  if (id) {
+    fillingForm(id);
+  }
+  modal.classList.add('d-block');
+};
+
+export const closeModal = () => {
+  modal.classList.remove('d-block');
+  form.reset();
+  form.identificator.value = '';
+  form.imagesave.value = '';
+  hidePreview();
+};
+
+
+export const modalController = ({ btn, delegation }) => {
+  if (btn) {
+    btn.addEventListener('click', () => {
+      openModal();
+      modalTitle.textContent = 'Добавить новый товар';
+      modalSubmitBtn.textContent = 'Добавить товар';
+    })
+  };
+
+  if (delegation) {
+    delegation.parent.addEventListener('click', ({ target }) => {
+      const goodsRow = target.closest(delegation.target);
+      const targetExclude = target.closest(delegation.targetExclude);
+      if (goodsRow && !targetExclude) {
+        modalTitle.textContent = `Изменить товар № ${goodsRow.dataset.id}`;
+        modalSubmitBtn.textContent = 'Изменить товар';
+        openModal(goodsRow.dataset.id);
+      }
+    })
+  };
+
+  modal.addEventListener('click', ({ target }) => {
+    if (target === modal || target.classList.contains('btn-close')) {
+      closeModal(modal);
+    }
+  })
+};
